080a414f34fb862006ce94d447ccb9cecd97914c
walon <walon@code-philosophy.com>
2025-04-17 10:05:16 +0800

    dnlib插件由源码换成dll，避免与hybridclr冲突

--- Plugins/dnlib/Protection/IEncryptionInstruction.cs
+++
@@ -1,7 +0,0 @@
-namespace dnlib.Protection {
-	public interface IEncryptionInstruction {
-		void Encrypt(byte[] content, uint start, uint length, byte[] encryptionParam);
-
-		string GenerateDecryptExpression(string dataVarName, string dataLengthVarName, string keyVarName);
-	}
-}
