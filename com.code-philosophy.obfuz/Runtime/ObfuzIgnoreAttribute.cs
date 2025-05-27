cd99bfe4f32bdef5c01a971699a03fb9fc22e799
walon <walon@code-philosophy.com>
2025-05-27 10:18:13 +0800

    ObufzScope拆分为独立代码文件

--- com.code-philosophy.obfuz/Runtime/ObfuzIgnoreAttribute.cs
+++ com.code-philosophy.obfuz/Runtime/ObfuzIgnoreAttribute.cs
@@ -6,25 +6,6 @@ using System.Threading.Tasks;
 
 namespace Obfuz
 {
-    [Flags]
-    public enum ObfuzScope
-    {
-        None = 0x0,
-        TypeName = 0x1,
-        Field = 0x2,
-        MethodName = 0x4,
-        MethodParameter = 0x8,
-        MethodBody = 0x10,
-        Method = MethodName | MethodParameter | MethodBody,
-        PropertyName = 0x20,
-        PropertyGetterSetterName = 0x40,
-        Property = PropertyName | PropertyGetterSetterName,
-        EventName = 0x100,
-        EventAddRemoveFireName = 0x200,
-        Event = EventName | PropertyGetterSetterName,
-        Module = 0x1000,
-        All = TypeName | Field | Method | Property | Event,
-    }
 
     [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
     public class ObfuzIgnoreAttribute : Attribute
