260f8b8cc54423614ad464d0c2cabe198268516d
walon <walon@code-philosophy.com>
2025-06-09 22:57:40 +0800

    新增InstinctPass，将 ObfuscationInstincts::FullNameOf和NameOf函数转为混淆前的原始类全名和类名

---
+++ Runtime/ObfuscationInstincts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obfuz
+{
+    public static class ObfuscationInstincts
+    {
+        /// <summary>
+        /// Returns the original full name before obfuscated of the type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static string FullNameOf<T>()
+        {
+            return typeof(T).FullName;
+        }
+
+        /// <summary>
+        /// Returns the original name before obfuscated of the type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static string NameOf<T>()
+        {
+            return typeof(T).Name;
+        }
+    }
+}
